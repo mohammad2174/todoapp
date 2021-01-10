a4956e461bd6b295dd499c90cdbf0946542ba947
mohammadreza <mohammadreza.khorrami21@gmail.com>
2021-01-10 14:04:17 +0330

    add todoapp

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class App extends Component {
+    render() {
+        return(
+            <h2>hello</h2>
+        )
+    }
+}
+
+
+
+export default App;

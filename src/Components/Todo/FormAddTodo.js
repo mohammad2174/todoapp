ac6d28741ad6c7550da24b1ba1fcd71b2f5993ad
mohammadreza <mohammadreza.khorrami21@gmail.com>
2021-01-12 13:50:36 +0330

    component consumer

--- src/Components/Todo/FormAddTodo.js
+++ src/Components/Todo/FormAddTodo.js
@@ -1,24 +1,29 @@
 import React, {useState} from 'react';
+import TodosContext from './../../Context/todos';
 
 
 function FormAddTodo(props){
 
     const [text , setText] = useState('');
-   let formHandler = e => {
+   let formHandler = (e , context) => {
         e.preventDefault();
-        props.add(text);
+       context.add(text);
         setText('');
     }
 
     let inputHandler = e => setText(e.target.value)
 
     return(
-        <form className="form-inline" onSubmit={formHandler}>
-            <div className="form-group">
-                <input type="text" className="form-control mx-sm-3" placeholder="i want to do ..." value={text} onChange={inputHandler}/>
-                <button type='submit' className="btn btn-primary">add</button>
-            </div>
-        </form>
+        <TodosContext.Consumer>
+            { context => (
+                <form className="form-inline" onSubmit={(e) => formHandler(e, context)}>
+                    <div className="form-group">
+                        <input type="text" className="form-control mx-sm-3" placeholder="i want to do ..." value={text} onChange={inputHandler}/>
+                        <button type='submit' className="btn btn-primary">add</button>
+                    </div>
+                </form>
+            )}
+        </TodosContext.Consumer>
     )
 }
 
--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import EditTodo from "./EditTodo";
+import TodosContext from './../../Context/todos';
 
 function Todo(props) {
     const {item} = props;
@@ -9,27 +10,31 @@ function Todo(props) {
         setEdit(false);
     }
     return (
-        <>
-            {
-                ! edit
-                ? (
-                        <div className="col-6 mb-2">
-                            <div
-                                className="d-flex justify-content-between align-items-center border rounded p-3">
-                                <div>
-                                    {item.text}
+        <TodosContext.Consumer>
+            { context => (
+                <>
+                    {
+                        ! edit
+                            ? (
+                                <div className="col-6 mb-2">
+                                    <div
+                                        className="d-flex justify-content-between align-items-center border rounded p-3">
+                                        <div>
+                                            {item.text}
+                                        </div>
+                                        <div>
+                                            <button type="button" className={`btn-sm mr-1 ${ !item.done ? 'btn-success' : 'btn-warning'}`} onClick={() => context.done(item.key)}>{item.done ? 'undone' : 'done'}</button>
+                                            <button type="button" className="btn btn-info btn-sm mr-1" onClick={() => setEdit(true)}>edit</button>
+                                            <button type="button" className="btn btn-danger btn-sm" onClick={() => context.delete(item.key)}>delete</button>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div>
-                                    <button type="button" className={`btn-sm mr-1 ${ !item.done ? 'btn-success' : 'btn-warning'}`} onClick={() => props.done(item.key)}>{item.done ? 'undone' : 'done'}</button>
-                                    <button type="button" className="btn btn-info btn-sm mr-1" onClick={() => setEdit(true)}>edit</button>
-                                    <button type="button" className="btn btn-danger btn-sm" onClick={() => props.delete(item.key)}>delete</button>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                    : <EditTodo text={item.text} edit={editHandler} />
-            }
-        </>
+                            )
+                            : <EditTodo text={item.text} edit={editHandler} />
+                    }
+                </>
+            )}
+        </TodosContext.Consumer>
     )
 }
 

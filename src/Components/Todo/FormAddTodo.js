99a0688b918f2e8aa826f956e04c5f4c4f09f665
mohammadreza <mohammadreza.khorrami21@gmail.com>
2021-01-13 10:00:56 +0330

    use Context Type in class base component

--- src/Components/Todo/FormAddTodo.js
+++ src/Components/Todo/FormAddTodo.js
@@ -1,27 +1,49 @@
-import React, {useState , useContext} from 'react';
+import React from 'react';
 import TodosContext from './../../Context/todos';
 
 
-function FormAddTodo(props){
+// function FormAddTodo(props){
+//
+//     const [text , setText] = useState('');
+//     const todosContext = useContext(TodosContext);
+//    let formHandler = e => {
+//         e.preventDefault();
+//        todosContext.add(text);
+//         setText('');
+//     }
+//
+//     let inputHandler = e => setText(e.target.value)
+//
+//     return(
+//         <form className="form-inline" onSubmit={formHandler}>
+//             <div className="form-group">
+//                 <input type="text" className="form-control mx-sm-3" placeholder="i want to do ..." value={text} onChange={inputHandler}/>
+//                 <button type='submit' className="btn btn-primary">add</button>
+//             </div>
+//         </form>
+//     )
+// }
 
-    const [text , setText] = useState('');
-    const todosContext = useContext(TodosContext);
-   let formHandler = e => {
+class FormAddTodo extends React.Component {
+    state = {text : ''}
+    static contextType = TodosContext;
+     formHandler(e) {
         e.preventDefault();
-       todosContext.add(text);
-        setText('');
+        this.context.add(this.state.text);
+         this.setState({text : ''});
     }
 
-    let inputHandler = e => setText(e.target.value)
-
-    return(
-        <form className="form-inline" onSubmit={formHandler}>
-            <div className="form-group">
-                <input type="text" className="form-control mx-sm-3" placeholder="i want to do ..." value={text} onChange={inputHandler}/>
-                <button type='submit' className="btn btn-primary">add</button>
-            </div>
-        </form>
-    )
+     inputHandler(e) { this.setState({text : e.target.value}) }
+    render() {
+        return(
+            <form className="form-inline" onSubmit={this.formHandler.bind(this)}>
+                <div className="form-group">
+                    <input type="text" className="form-control mx-sm-3" placeholder="i want to do ..." value={this.state.text} onChange={this.inputHandler.bind(this)}/>
+                    <button type='submit' className="btn btn-primary">add</button>
+                </div>
+            </form>
+        )
+    }
 }
 
 

006427a8636f70e459cb0201881a8e13af9b4131
mohammadreza <mohammadreza.khorrami21@gmail.com>
2021-01-13 09:38:10 +0330

    use hook context

--- src/Components/Todo/FormAddTodo.js
+++ src/Components/Todo/FormAddTodo.js
@@ -1,29 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState , useContext} from 'react';
 import TodosContext from './../../Context/todos';
 
 
 function FormAddTodo(props){
 
     const [text , setText] = useState('');
-   let formHandler = (e , context) => {
+    const todosContext = useContext(TodosContext);
+   let formHandler = e => {
         e.preventDefault();
-       context.add(text);
+       todosContext.add(text);
         setText('');
     }
 
     let inputHandler = e => setText(e.target.value)
 
     return(
-        <TodosContext.Consumer>
-            { context => (
-                <form className="form-inline" onSubmit={(e) => formHandler(e, context)}>
-                    <div className="form-group">
-                        <input type="text" className="form-control mx-sm-3" placeholder="i want to do ..." value={text} onChange={inputHandler}/>
-                        <button type='submit' className="btn btn-primary">add</button>
-                    </div>
-                </form>
-            )}
-        </TodosContext.Consumer>
+        <form className="form-inline" onSubmit={formHandler}>
+            <div className="form-group">
+                <input type="text" className="form-control mx-sm-3" placeholder="i want to do ..." value={text} onChange={inputHandler}/>
+                <button type='submit' className="btn btn-primary">add</button>
+            </div>
+        </form>
     )
 }
 
--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -1,40 +1,37 @@
-import React, {useState} from 'react';
+import React, {useState , useContext} from 'react';
 import EditTodo from "./EditTodo";
 import TodosContext from './../../Context/todos';
 
 function Todo(props) {
     const {item} = props;
     const [ edit, setEdit ] = useState(false);
+    const todosContext = useContext(TodosContext);
     let editHandler = text => {
-        props.edit(item.key,text);
+        todosContext.edit(item.key,text);
         setEdit(false);
     }
     return (
-        <TodosContext.Consumer>
-            { context => (
-                <>
-                    {
-                        ! edit
-                            ? (
-                                <div className="col-6 mb-2">
-                                    <div
-                                        className="d-flex justify-content-between align-items-center border rounded p-3">
-                                        <div>
-                                            {item.text}
-                                        </div>
-                                        <div>
-                                            <button type="button" className={`btn-sm mr-1 ${ !item.done ? 'btn-success' : 'btn-warning'}`} onClick={() => context.done(item.key)}>{item.done ? 'undone' : 'done'}</button>
-                                            <button type="button" className="btn btn-info btn-sm mr-1" onClick={() => setEdit(true)}>edit</button>
-                                            <button type="button" className="btn btn-danger btn-sm" onClick={() => context.delete(item.key)}>delete</button>
-                                        </div>
-                                    </div>
+        <>
+            {
+                ! edit
+                    ? (
+                        <div className="col-6 mb-2">
+                            <div
+                                className="d-flex justify-content-between align-items-center border rounded p-3">
+                                <div>
+                                    {item.text}
                                 </div>
-                            )
-                            : <EditTodo text={item.text} edit={editHandler} />
-                    }
-                </>
-            )}
-        </TodosContext.Consumer>
+                                <div>
+                                    <button type="button" className={`btn-sm mr-1 ${ !item.done ? 'btn-success' : 'btn-warning'}`} onClick={() => todosContext.done(item.key)}>{item.done ? 'undone' : 'done'}</button>
+                                    <button type="button" className="btn btn-info btn-sm mr-1" onClick={() => setEdit(true)}>edit</button>
+                                    <button type="button" className="btn btn-danger btn-sm" onClick={() => todosContext.delete(item.key)}>delete</button>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                    : <EditTodo text={item.text} edit={editHandler} />
+            }
+        </>
     )
 }
 

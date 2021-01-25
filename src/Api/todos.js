5c2128ae9fee15a5441d65f135c218ac7e0d5758
mohammad <mohammadreza.khorrami21@gmail.com>
2021-01-25 14:21:45 +0330

    work with Axios interceptors

--- src/Api/todos.js
+++ src/Api/todos.js
@@ -6,5 +6,17 @@ const instanse = axios.create({
     timeout : 5000
 });
 
+axios.interceptors.request.use(function (config) {
+    return config;
+}, function (err) {
+    return Promise.reject(err)
+})
+
+axios.interceptors.response.use(function (response) {
+    return response;
+}, function (err) {
+    return Promise.reject(err)
+})
+
 
 export default instanse;
